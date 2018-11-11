d20eb0f4135a3dbb5b071774c144a9798212cd33
trungntm <trungntm.97@gmail.com>
2018-11-12 01:38:06 +0700

    commit

--- api/v1.0/product/index.js
+++ api/v1.0/product/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('./product');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,10 +10,22 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-    const product = {
+    // const product = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // }
+
+    let product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
+    });
+    product.save().then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log("ERROR INSERT : " + err);
+    });
+
     res.status(201).json({
         message: 'Handling POST request /products',
         createdProduct: product
--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const bodyParser = require('body-parser');
 const productRoutes = require('./api/v1.0/product');
 const orderRoutes = require('./api/v1.0/order');
 
+const uri = 'mongodb://trungntm:250303022602asd@ds111258.mlab.com:11258/mongo_node_demo';
+const mongoClient = require('mongodb').MongoClient;
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+mongoClient.connect(uri, { useNewUrlParser: true }, (err, db) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(`Connect mongodb successfully ...`);
+})
 
 app.use((req, res, next) => {
     req.header(`Access-Control-Allow-Origin`, `*`);
