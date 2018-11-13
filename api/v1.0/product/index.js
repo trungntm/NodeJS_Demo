11e9dff0bd12662e2056589584c406f4b3e9ea55
trungntm <trungntm.97@gmail.com>
2018-11-14 01:29:56 +0700

    update response

--- api/v1.0/product/index.js
+++ api/v1.0/product/index.js
@@ -5,14 +5,30 @@ const Product = require('./product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select(`name price _id`)
         .exec()
         .then(docs => {
             console.log(docs);
-            if (docs.length > 0) {
-                res.status(200).json(docs);
-            } else {
-                res.status(404).json({ message: "No product found" });
-            }
+            // if (docs.length > 0) {
+            //     res.status(200).json(docs);
+            // } else {
+            //     res.status(404).json({ message: "No product found" });
+            // }
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        name: doc.name,
+                        price: doc.price,
+                        request: {
+                            type: 'GET',
+                            url: `http://localhost:3000/products/${doc._id}`
+                        }
+                    }
+                })
+            };
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
